af0a515145b256a63a0e1ab1e3eb3449b59fc668
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-10-29 14:53:29 +0800

    Typos in config file test1.py

--- configurations/test1.py
+++ configurations/test1.py
@@ -1,4 +1,6 @@
-import lasagne
+import lasagne as nn
+import utils
+import theano.tensor as T
 
 #validate_every = 40
 start_saving_at = 0
@@ -18,12 +20,12 @@ learning_rate_schedule = {
 }
 
 def build_model():
-    l_in = lasagne.layers.InputLayer((None, 1, 64, 1))
-    l_conv1_a = lasagne.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(3,1), nonlinearity=nn.nonlinearities.leaky_rectify)
-    l_conv1_b = lasagne.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(5,1), nonlinearity=nn.nonlinearities.leaky_rectify)
-    l_conv1_c = lasagne.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(7,1), nonlinearity=nn.nonlinearities.leaky_rectify)
-    l_con_a = lasage.layers.ConcatLayer([l_conv1_a, l_conv1_b, l_conv1_c], axis=2)
-    l_h1 = lasagne.layers.DenseLayer(nn.layers.DropoutLayer(l_con_a), num_units=150, nonlinearity=nn.nonlinearities.leaky_rectify)
-    l_out = lasagne.layers.DenseLayer(l_h1, num_units=1, nonlinearity=utils.softmax)
+    l_in = nn.layers.InputLayer((None, 1, 64, 1))
+    l_conv1_a = nn.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(3,1), nonlinearity=nn.nonlinearities.leaky_rectify)
+    l_conv1_b = nn.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(5,1), nonlinearity=nn.nonlinearities.leaky_rectify)
+    l_conv1_c = nn.layers.Conv2DLayer(l_in, num_filters=8, filter_size=(7,1), nonlinearity=nn.nonlinearities.leaky_rectify)
+    l_con_a = nn.layers.ConcatLayer([l_conv1_a, l_conv1_b, l_conv1_c], axis=2)
+    l_h1 = nn.layers.DenseLayer(nn.layers.DropoutLayer(l_con_a), num_units=150, nonlinearity=nn.nonlinearities.leaky_rectify)
+    l_out = nn.layers.DenseLayer(l_h1, num_units=1, nonlinearity=utils.softmax)
     
     return l_in, l_out
