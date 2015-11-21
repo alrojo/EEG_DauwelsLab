666c08cb0e5a0336faeaaa50feac02537c7426a0
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-11-21 21:20:56 +0800

    batch_script

--- my_script.sh
+++ my_script.sh
@@ -7,4 +7,6 @@
 #PBS -m abe
 if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 module load cuda
-python john.py 5
+python train.py ConvMulti 1 csv 300
+python train.py ConvMulti 2 csv 300
+python train.py ConvMulti 3 csv 300
