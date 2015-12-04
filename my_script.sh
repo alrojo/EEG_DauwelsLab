1762010e9406cb7a9db2876dfdf6388e2a32de83
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-12-04 13:28:00 +0800

    new job

--- my_script.sh
+++ my_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#PBS -N test5
+#PBS -N test1AndMLP
 #PBS -q k40_interactive
 #PBS -l walltime=36:00:00
 #PBS -l nodes=1:ppn=4:gpus=1
@@ -7,11 +7,19 @@
 #PBS -m abe
 if test X$PBS_ENVIRONMENT = XPBS_BATCH; then cd $PBS_O_WORKDIR; fi
 module load cuda
-python train.py test1 1 300
-python train.py test1 2 300
-python train.py test1 3 300
-python train.py test1 4 300
-python train.py test1 5 300
-python train.py test1 6 300
-python train.py test1 7 300
-python train.py test1 8 300
+python train.py test1 1 200
+python train.py test1 2 200
+python train.py test1 3 200
+python train.py test1 4 200
+python train.py test1 5 200
+python train.py test1 6 200
+python train.py test1 7 200
+python train.py test1 8 200
+python train.py MLP 1 200
+python train.py MLP 2 200
+python train.py MLP 3 200
+python train.py MLP 4 200
+python train.py MLP 5 200
+python train.py MLP 6 200
+python train.py MLP 7 200
+python train.py MLP 8 200
