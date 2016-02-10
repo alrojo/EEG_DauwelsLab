0c2a9dd935193ad4ac814c866d1e58ec8e29dd7a
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-02-11 00:07:50 +0100

    batch_script

--- batch_script.sh
+++ batch_script.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-python train.py T1 2 150 
-python train.py T1 3 150
-python train.py T1 4 150
-python train.py T1 5 150
-python train.py T1 6 150
-python train.py T1 7 150
-python train.py T1 8 150
+python train.py LSTM 1 150
+python train.py LSTM 2 150 
+python train.py LSTM 3 150
+python train.py LSTM 4 150
+python train.py LSTM 5 150
+python train.py LSTM 6 150
+python train.py LSTM 7 150
+python train.py LSTM 8 150
