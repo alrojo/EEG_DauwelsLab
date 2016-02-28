ae7224985620990c68e7bca5fa03d1ee30383adf
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-02-28 19:56:24 +0100

    logistic

--- configurations/LogisticRegression.py
+++ configurations/LogisticRegression.py
@@ -1,25 +1,38 @@
-import lasagne as nn
-import utils
-
+import lasagne
+#from BatchNormLayer import batch_norm
 #validate_every = 40
 start_saving_at = 0
-save_every = 20
+save_every = 100
 #write_every_batch = 10
 
+recurrent=True
 epochs = 300
-batch_size = 128
+batch_size = 2048
+N_L1 = 100
+N_LSTM_F = 100
+n_inputs = 1
+num_classes = 1
+seq_len = 64
 optimizer = "rmsprop"
-lambda_reg = 0.0001
+lambda_reg = 0.0
 cut_grad = 20
 
 learning_rate_schedule = {
-    0: 0.001,
-    250: 0.0005,
-    275: 0.00025,
+	0: 0.001,
+	250: 0.0005,
+	275: 0.00025,
 }
 
 def build_model():
-    l_in = nn.layers.InputLayer((None, 1, 64, 1))
-    l_out = nn.layers.DenseLayer(l_in, num_units=1, nonlinearity=utils.sigmoid)
-    
-    return l_in, l_out
+	# 1. Input layer
+	l_in = lasagne.layers.InputLayer(shape=(None, seq_len, n_inputs))
+
+	# 2. LSTM Layer
+#	l_forward = lasagne.layers.LSTMLayer(l_in, N_LSTM_F)
+#	l_forward_last = lasagne.layers.SliceLayer(l_forward, indices=-1, axis=1)
+
+	# 3. Output Layer
+	l_out = lasagne.layers.DenseLayer(
+	l_in, num_units=num_classes, nonlinearity=lasagne.nonlinearities.sigmoid)
+
+	return l_in, l_out
