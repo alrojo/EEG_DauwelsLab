d569c5e592f168f9bc5f1b9f52280d37389fd84f
Alexander Rosenberg Johansen <alexander@herhjemme.dk>
2016-03-04 10:40:05 +0100

    predictions and batch_script update

---
+++ batch_predictions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python predict.py metadata/FOR_ENSEMBLE/ 1
+python predict.py metadata/FOR_ENSEMBLE/ 2
+python predict.py metadata/FOR_ENSEMBLE/ 3
+python predict.py metadata/FOR_ENSEMBLE/ 4
+python predict.py metadata/FOR_ENSEMBLE/ 5
+python predict.py metadata/FOR_ENSEMBLE/ 6
+python predict.py metadata/FOR_ENSEMBLE/ 7
+python predict.py metadata/FOR_ENSEMBLE/ 8
--- batch_script2.sh
+++ batch_script2.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-python train.py LSTM 1 150
-python train.py LSTM 2 150 
-python train.py LSTM 3 150
-python train.py LSTM 4 150
 python train.py LSTM 5 150
 python train.py LSTM 6 150
 python train.py LSTM 7 150
