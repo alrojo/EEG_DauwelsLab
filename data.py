c908888a30bb5c82d7a3e0d6145dec539cf84603
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-04-24 12:36:41 +0200

    for new folders

--- data.py
+++ data.py
@@ -6,9 +6,6 @@ import glob
 # User libs
 import utils
 
-paths_csv = "./data/csv/*"
-
-# to dir has to be as "numpy"
 def convert_data(paths_from):
 	file_paths = glob.glob(paths_from)
 	if file_paths == []:
@@ -23,25 +20,22 @@ def convert_data(paths_from):
 
 def load_data(CVsplit):
 	print "loadData started!"
-#	if(len(glob.glob('./data/csv/*'))!=len(glob.glob('./data/numpy/*'))):
-#		print "converting data ..."
-#		convert_data();
-	xb_train = utils.load_gz('data/numpy_train_valid/Btrn%s.npy.gz' % CVsplit).astype('float32')
+	xb_train = utils.load_gz('data/numpy/train/Btrn%s.npy.gz' % CVsplit).astype('float32')
 	tb_train = np.zeros((xb_train.shape[0],1), dtype='float32')
-	xs_train = utils.load_gz('data/numpy_train_valid/Strn%s.npy.gz' % CVsplit).astype('float32')
+	xs_train = utils.load_gz('data/numpy/train/Strn%s.npy.gz' % CVsplit).astype('float32')
 	ts_train = np.ones((xs_train.shape[0],1), dtype='float32')
-	xb_valid = utils.load_gz('data/numpy_train_valid/Bval%s.npy.gz' % CVsplit).astype('float32')
+	xb_valid = utils.load_gz('data/numpy/train/Bval%s.npy.gz' % CVsplit).astype('float32')
 	tb_valid = np.zeros((xb_valid.shape[0],1), dtype='float32')
-	xs_valid = utils.load_gz('data/numpy_train_valid/Sval%s.npy.gz' % CVsplit).astype('float32')
+	xs_valid = utils.load_gz('data/numpy/train/Sval%s.npy.gz' % CVsplit).astype('float32')
 	ts_valid = np.ones((xs_valid.shape[0],1), dtype='float32')
 
 	return xb_train, xb_valid, tb_train, tb_valid, xs_train, xs_valid, ts_train, ts_valid
 
 def load_test(CVsplit):
 	print "loadTest started!"
-	xb_test = utils.load_gz('data/numpy_test/Btst%s.npy.gz' % CVsplit).astype('float32')
+	xb_test = utils.load_gz('data/numpy/test/Btst%s.npy.gz' % CVsplit).astype('float32')
 	tb_test = np.zeros((xb_test.shape[0],1), dtype='float32')
-	xs_test = utils.load_gz('data/numpy_test/Stst%s.npy.gz' % CVsplit).astype('float32')
+	xs_test = utils.load_gz('data/numpy/test/Stst%s.npy.gz' % CVsplit).astype('float32')
 	ts_test = np.ones((xs_test.shape[0],1), dtype='float32')
 
 	return xb_test, tb_test, xs_test, ts_test
