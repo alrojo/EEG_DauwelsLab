47160f7f262a6df93702d47ea5aa7a46ed9a6971
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-04-26 11:08:19 +0200

    debugging

--- debug_metadata.py
+++ debug_metadata.py
@@ -10,13 +10,11 @@ import matplotlib.pyplot as plt
 import glob
 
 if not (len(sys.argv) == 4):
-    sys.exit("Usage: python debug_metadata.py <path> <train/test> <topX>")
+    sys.exit("Usage: python debug_metadata.py <path> <topX>")
 
 RNNs = glob.glob(sys.argv[1] + "*")
 print(RNNs)
-test = sys.argv[2]
-topX = int(sys.argv[3])
-#metadata_path = sys.argv[3]
+topX = int(sys.argv[2])
 
 #MLPs = glob.glob("./metadata/*MLP*")
 #Convs = glob.glob("./metadata/save/*")
@@ -28,39 +26,29 @@ for metadata_paths, name, colours in metasets: # colours not used anymore
     print name
     all_aucs_train = []
     all_aucs_valid = []
-#    all_aucs_test = []
     all_roc_tprs_train = []
     all_roc_tprs_valid = []
-#    all_roc_tprs_test = []
     all_roc_fprs_train = []
     all_roc_fprs_valid = []
-#    all_roc_fprs_test = []
     all_roc_thresholds_train = []
     all_roc_thresholds_valid = []
-#    all_roc_thresholds_test = []
     for metadata_path in metadata_paths:
         print "----"
 #        print "Loading metadata file %s" % metadata_path
         metadata = np.load(metadata_path)
         acc_eval_valid = metadata['accuracy_eval_valid']
         acc_eval_train = metadata['accuracy_eval_train']
-#        acc_eval_test = metadata['accuracy_eval_test']
         auc_eval_valid = metadata['auc_eval_valid']
         auc_eval_train = metadata['auc_eval_train']
-#        auc_eval_test = metadata['auc_eval_test']
         roc_tpr_eval_valid = metadata['roc_tpr_eval_valid']
         roc_tpr_eval_train = metadata['roc_tpr_eval_train']
-#        roc_tpr_eval_test = metadata['roc_tpr_eval_test']
         roc_fpr_eval_valid = metadata['roc_fpr_eval_valid']
         roc_fpr_eval_train = metadata['roc_fpr_eval_train']
-#        roc_fpr_eval_test = metadata['roc_fpr_eval_test']
         roc_thresholds_eval_valid = metadata['roc_thresholds_eval_valid']
         roc_thresholds_eval_train = metadata['roc_thresholds_eval_train']
-#        roc_thresholds_eval_test = metadata['roc_thresholds_eval_test']
         
         # Running through top X best and making an averaged top X validations
         best_val = np.zeros(shape=topX, dtype='float32')
-#        best_test = np.zeros(shape=topX, dtype='float32')
         max_val = np.zeros(shape=topX, dtype=int)
         for idx, tup in enumerate(zip(auc_eval_train, auc_eval_valid)):
             x, v = tup
@@ -71,20 +59,13 @@ for metadata_paths, name, colours in metasets: # colours not used anymore
             print max_val[i]
             all_aucs_train.append(auc_eval_train[max_val[i]])
             all_aucs_valid.append(auc_eval_valid[max_val[i]])
-#            all_aucs_test.append(auc_eval_test[max_val[i]])
             all_roc_tprs_train.append(roc_tpr_eval_train[max_val[i]])
             all_roc_tprs_valid.append(roc_tpr_eval_valid[max_val[i]])
-#            all_roc_tprs_test.append(roc_tpr_eval_test[max_val[i]])
             all_roc_fprs_train.append(roc_fpr_eval_train[max_val[i]])
             all_roc_fprs_valid.append(roc_fpr_eval_valid[max_val[i]])
-#            all_roc_fprs_test.append(roc_fpr_eval_test[max_val[i]])
             all_roc_thresholds_train.append(roc_thresholds_eval_train[max_val[i]])
             all_roc_thresholds_valid.append(roc_thresholds_eval_valid[max_val[i]])
-#            all_roc_thresholds_test.append(roc_thresholds_eval_test[max_val[i]])
-            if test == "test":
-                print "Train = %.5f - Validation = %.5f - Test = %.5f" %(np.asarray(auc_eval_train[max_val[i]]), np.asarray(auc_eval_valid[max_val[i]]), np.asarray(auc_eval_test[max_val[i]]))
-            else:
-                print "Train = %.5f - Validation = %.5f" %(np.asarray(all_aucs_train).mean(), np.asarray(all_aucs_valid).mean())
+            print "Train = %.5f - Validation = %.5f" %(np.asarray(all_aucs_train).mean(), np.asarray(all_aucs_valid).mean())
             auc_eval_valid[max_val[i]] = 0.0 # To make it take the next max value
 #    cols = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 #    for i, col in enumerate(cols):
@@ -99,10 +80,7 @@ for metadata_paths, name, colours in metasets: # colours not used anymore
 #    plt.subplot(222)
 #    plt.plot(all_roc_fprs_test[5], all_roc_tprs_test[5])
     print("@@@OVERALL@@@")
-    if test == "test":
-        print "Train = %.5f - Validation = %.5f - Test = %.5f" %(np.asarray(all_aucs_train).mean(), np.asarray(all_aucs_valid).mean(), np.asarray(all_aucs_test).mean())
-    else:
-        print "Train = %.5f - Validation = %.5f" %(np.asarray(all_aucs_train).mean(), np.asarray(all_aucs_valid).mean())
+    print "Train = %.5f - Validation = %.5f" %(np.asarray(all_aucs_train).mean(), np.asarray(all_aucs_valid).mean())
     print
 #plt.show()
 #            print max_val[i]
