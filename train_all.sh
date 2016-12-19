647c75701265b57a16e71af6f4ff4ba84e05d4d7
Ubuntu <ubuntu@ip-172-31-31-223.eu-west-1.compute.internal>
2016-12-19 19:03:44 +0000

    train script

---
+++ train_all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python train.py rnn_big 1
+python train.py rnn_big 2
+python train.py rnn_big 3
+python train.py rnn_big 4
+python train.py rnn_big 5
+python train.py rnn_big 6
+python train.py rnn_big 7
+python train.py rnn_big 8
