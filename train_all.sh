e4a540e92cea47a2148a7e708fc14a9b6b5cd5f6
Ubuntu <ubuntu@ip-172-31-31-223.eu-west-1.compute.internal>
2016-12-20 22:02:15 +0000

    train ensemble

--- train_all.sh
+++ train_all.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 python train.py rnn_big 1
+python train.py rnn_big2 1
 python train.py rnn_big 2
+python train.py rnn_big2 2
 python train.py rnn_big 3
+python train.py rnn_big2 3
 python train.py rnn_big 4
+python train.py rnn_big2 4
 python train.py rnn_big 5
+python train.py rnn_big2 5
 python train.py rnn_big 6
+python train.py rnn_big2 6
 python train.py rnn_big 7
+python train.py rnn_big2 7
 python train.py rnn_big 8
+python train.py rnn_big2 8
